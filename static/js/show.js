6fb9ecad0406be496feb57082520d6ea28a026d0
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-10-07 16:19:51 +0900

    show template effect update

---
+++ static/js/show.js
@@ -0,0 +1,19 @@
+// name 님은 effect
+const result_1 = document.getElementById("result_1");
+result_1.style.transition='all 1.5s';
+result_1.style.transform='translateX(0)';
+
+// major에 effect
+const result_2 = document.getElementById("result_2");
+result_2.style.transition='all 1.5s';
+result_2.style.transform='translateX(0)';
+
+// 모의지원중이며, effect
+const result_3 = document.getElementById("result_3");
+result_3.style.transition='all 1.5s';
+result_3.style.transform='translateX(0)';
+
+// 총 sum명 중 rank 등입니다. effect
+const result_4 = document.getElementById("result_4");
+result_4.style.transition='all 1.5s';
+result_4.style.transform='translateX(0)';
