4a4517c62a3eaff212b0f1ec8343628ce79a036c
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-10-07 16:34:41 +0900

    signin template effect implement

---
+++ static/js/signin.js
@@ -0,0 +1,9 @@
+// * 1학년 2학기에 재학중인 경우, 1학년 1학기까지 확정된 GPA를 적어주시기 바랍니다. effect
+const info_1 = document.getElementById("info_1");
+info_1.style.transition='all 1.5s';
+info_1.style.color='#fff';
+
+// * 첫 모의지원을 완료하는 즉시 kurooru@hufs.ac.kr 으로 성적, 본인 인증 메일을 보내 주시기 바랍니다. effect
+const info_2 = document.getElementById("info_2");
+info_2.style.transition='all 1.5s';
+info_2.style.color='#fff';
