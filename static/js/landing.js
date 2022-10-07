a0179255b7f80d361c3ad11d1cc5facde378a237
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-10-07 16:12:18 +0900

    landing template js effect implement

---
+++ static/js/landing.js
@@ -0,0 +1,19 @@
+// HUFS effect
+const title_1 = document.getElementById("title_1");
+title_1.style.transition='all 1.5s';
+title_1.style.transform='translateX(0)';
+
+// Apply effect
+const title_2 = document.getElementById("title_2");
+title_2.style.transition='all 1.5s';
+title_2.style.transform='translateX(0)';
+
+// 한국외대 이중전공 effect
+const title_3 = document.getElementById("title_3");
+title_3.style.transition='all 1.5s';
+title_3.style.transform='translateX(0)';
+
+// 모의지원 서비스 effect
+const title_4 = document.getElementById("title_4");
+title_4.style.transition='all 1.5s';
+title_4.style.transform='translateX(0)';
