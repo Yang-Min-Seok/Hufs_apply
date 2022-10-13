182eb44bc3c64eb436fca2e733796b657ae169df
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2022-10-13 15:57:26 +0900

    apply page effect update (transition 2s -> 3s)

--- static/js/apply.js
+++ static/js/apply.js
@@ -1,14 +1,14 @@
 // 희망학과에 effect
 const choose_1 = document.getElementById("choose_1");
-choose_1.style.transition='all 2s';
+choose_1.style.transition='all 3s';
 choose_1.style.color='#fff';
 
 // 지원해보세요! effect
 const choose_2 = document.getElementById("choose_2");
-choose_2.style.transition='all 2s';
+choose_2.style.transition='all 3s';
 choose_2.style.color='#fff';
 
 // 언제든 바꿀 수 있습니다! effect
 const choose_3 = document.getElementById("choose_3");
-choose_3.style.transition='all 2s';
+choose_3.style.transition='all 3s';
 choose_3.style.color='#fff';
